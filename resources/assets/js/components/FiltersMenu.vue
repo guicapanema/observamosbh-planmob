de98d0a28b8770116a52dfc7567689ccf202f600
Guilherme Capanema <capanema@infrequente.com.br>
2018-02-22 14:40:25 -0300

    Insere visualização de filtros ativos na visão em lista.

--- resources/assets/js/components/FiltersMenu.vue
+++ resources/assets/js/components/FiltersMenu.vue
@@ -1,82 +1,101 @@
 <template>
-	<nav class="panel">
-		<p class="panel-heading">
-			Filtros
-			<b-tooltip label="Visão em colunas" type="is-light" position="is-bottom" class="is-pulled-right">
-				<div class="view-button" @click="onColumn()">
-					<b-icon icon="columns" type="is-info" size="is-small"></b-icon>
-				</div>
-			</b-tooltip>
-		</p>
-		<div class="panel-block">
-			<p class="control has-icons-left">
-				<input v-model="filters['search']" class="input is-small" type="text" placeholder="buscar">
-				<span class="icon is-small is-left">
-					<i class="fa fa-search"></i>
-				</span>
+	<div>
+		<nav class="panel">
+			<p class="panel-heading">
+				Filtros
+				<b-tooltip label="Visão em colunas" type="is-light" position="is-bottom" class="is-pulled-right">
+					<div class="view-button" @click="onColumn()">
+						<b-icon icon="columns" type="is-info" size="is-small"></b-icon>
+					</div>
+				</b-tooltip>
 			</p>
-		</div>
-		<p class="panel-tabs">
-			<a :class="{'is-active': view === 'modals'}" @click="view = 'modals'">modais</a>
-			<a :class="{'is-active': view === 'tags'}" @click="view = 'tags'">tags</a>
-		</p>
-		<template v-if="view === 'modals'">
-			<a :class="{'panel-block': true,
-						'is-active': hasModal('pedestre')}"
-				@click="onSetModal('pedestre')">
-				<span class="panel-icon">
-					<i class="fa fa-female"></i>
-				</span>
-				pedestre
-			</a>
-			<a :class="{'panel-block': true,
-						'is-active': hasModal('bicicleta')}"
-				@click="onSetModal('bicicleta')">
-				<span class="panel-icon">
-					<i class="fa fa-bicycle"></i>
-				</span>
-				bicicleta
-			</a>
-			<a :class="{'panel-block': true,
-						'is-active': hasModal('onibus')}"
-				@click="onSetModal('onibus')">
-				<span class="panel-icon">
-					<i class="fa fa-bus"></i>
-				</span>
-				ônibus
-			</a>
-			<a :class="{'panel-block': true,
-						'is-active': hasModal('metro')}"
-				@click="onSetModal('metro')">
-				<span class="panel-icon">
-					<i class="fa fa-subway"></i>
-				</span>
-				metrô
-			</a>
-			<a :class="{'panel-block': true,
-						'is-active': hasModal('logistica')}"
-				@click="onSetModal('logistica')">
-				<span class="panel-icon">
-					<i class="fa fa-truck"></i>
-				</span>
-				logística
-			</a>
-		</template>
-		<template v-if="view === 'tags'">
-			<a  v-for="tag of tags"
-				:class="{'panel-block': true,
-						'is-active': hasTag(tag)}"
-				@click="onSetTag(tag)">
-				{{tag}}
-			</a>
-		</template>
-		<div class="panel-block">
-			<button class="button is-primary is-outlined is-fullwidth"
-					@click="onCleanFilters()">
-				limpar filtros
-			</button>
-		</div>
-	</nav>
+			<div class="panel-block">
+				<p class="control has-icons-left">
+					<input v-model="filters['search']" class="input is-small" type="text" placeholder="buscar">
+					<span class="icon is-small is-left">
+						<i class="fa fa-search"></i>
+					</span>
+				</p>
+			</div>
+			<p class="panel-tabs">
+				<a :class="{'is-active': view === 'modals'}" @click="view = 'modals'">modais</a>
+				<a :class="{'is-active': view === 'tags'}" @click="view = 'tags'">tags</a>
+			</p>
+			<template v-if="view === 'modals'">
+				<a :class="{'panel-block': true,
+							'is-active': hasModal('pedestre')}"
+					@click="onSetModal('pedestre')">
+					<span class="panel-icon">
+						<i class="fa fa-female"></i>
+					</span>
+					pedestre
+				</a>
+				<a :class="{'panel-block': true,
+							'is-active': hasModal('bicicleta')}"
+					@click="onSetModal('bicicleta')">
+					<span class="panel-icon">
+						<i class="fa fa-bicycle"></i>
+					</span>
+					bicicleta
+				</a>
+				<a :class="{'panel-block': true,
+							'is-active': hasModal('onibus')}"
+					@click="onSetModal('onibus')">
+					<span class="panel-icon">
+						<i class="fa fa-bus"></i>
+					</span>
+					ônibus
+				</a>
+				<a :class="{'panel-block': true,
+							'is-active': hasModal('metro')}"
+					@click="onSetModal('metro')">
+					<span class="panel-icon">
+						<i class="fa fa-subway"></i>
+					</span>
+					metrô
+				</a>
+				<a :class="{'panel-block': true,
+							'is-active': hasModal('logistica')}"
+					@click="onSetModal('logistica')">
+					<span class="panel-icon">
+						<i class="fa fa-truck"></i>
+					</span>
+					logística
+				</a>
+			</template>
+			<template v-if="view === 'tags'">
+				<a  v-for="tag of tags"
+					:class="{'panel-block': true,
+							'is-active': hasTag(tag)}"
+					@click="onSetTag(tag)">
+					{{tag}}
+				</a>
+			</template>
+
+		</nav>
+		<nav class="panel" v-if="hasFilters">
+			<p class="panel-heading">
+				Filtros ativos
+			</p>
+			<div class="panel-block">
+				<div class="field is-grouped is-grouped-multiline">
+					<div v-for="(filter, filterName) in filters" v-if="filter.length > 0" class="control">
+						<div class="tags has-addons">
+							<span class="tag">{{filterName}}</span>
+							<span v-for="value in filter" class="tag is-info">{{value}}</span>
+						</div>
+					</div>
+				</div>
+				<br />
+			</div>
+			<div class="panel-block">
+				<button class="button is-primary is-outlined is-fullwidth"
+						@click="onCleanFilters()">
+					limpar filtros
+				</button>
+			</div>
+		</nav>
+	</div>
 </template>
 
 <script>
@@ -88,6 +107,18 @@
 		data() {
 			return {
 				view: 'modals'
+			}
+		},
+
+		computed: {
+			hasFilters() {
+				let hasFilters = false;
+				for (let filter in this.filters) {
+					if(this.filters[filter].length > 0) {
+						hasFilters = true;
+					}
+				}
+				return hasFilters;
 			}
 		},
 
