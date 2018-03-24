aa3f69bfd339e50255cd1200efebf3eb3c50058e
Guilherme Capanema <capanema@infrequente.com.br>
2018-03-24 10:31:09 -0300

    Adiciona verificações e mensagem de sucesso ao formulário de contato.

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -9,11 +9,21 @@ class ContactController extends Controller
 {
 
 	public function contact(Request $request) {
-		Mail::raw(request('mensagem'), function($message) {
-		$message->from(request('email'), 'Formulário de contato mobilidadebh.org');
 
-		$message->to('guilherme@capanema.me');
+		$this->validate(request(), [
+			'nome' => 'required|string|max:255',
+			'email' => 'required|string|email|max:255',
+			'mensagem' => 'required|string'
+		]);
+
+		$messageBody = "Mensagem de: " . request('nome') . " (" . request('email') . ")\r\n\r\n" . request('mensagem');
+		Mail::raw($messageBody, function($message) {
+			$message->from('contato@mobilidadebh.org', 'Formulário de contato mobilidadebh.org');
+			$message->to(getenv('MAIL_CONTACT_TO'));
+			$message->replyTo(request('email'), request('nome'));
+			$message->subject('Nova mensagem no mobilidadebh.org');
 		});
+		$request->session()->flash('success', 'Mensagem enviada com sucesso!');
 		return view('contato');
 	}
 }
--- resources/views/contato.blade.php
+++ resources/views/contato.blade.php
@@ -17,28 +17,33 @@
 					<div class="field">
 						<label class="label">Nome</label>
 						<div class="control has-icons-left">
-							<input class="input" type="text" id="nome" name="nome" placeholder="Digite seu nome">
+							<input class="input{{ $errors->has('nome') ? ' is-danger' : '' }}" type="text" id="nome" name="nome" placeholder="Digite seu nome">
 							<span class="icon is-small is-left">
 								<i class="fas fa-user"></i>
 							</span>
 						</div>
+						@if ($errors->has('nome'))<p class="help is-danger">Por favor, digite seu nome</p>@endif
 					</div>
 
 					<div class="field">
 						<label class="label">Email</label>
 						<div class="control has-icons-left">
-							<input class="input" type="email" id="email" name="email" placeholder="Digite seu email">
+							<input class="input{{ $errors->has('email') ? ' is-danger' : '' }}" type="email" id="email" name="email" placeholder="Digite seu email">
 							<span class="icon is-small is-left">
 								<i class="fas fa-envelope"></i>
 							</span>
 						</div>
+						@if ($errors->has('email'))<p class="help is-danger">Por favor, digite um e-mail válido</p>@endif
+
 					</div>
 
 					<div class="field">
 						<label class="label">Mensagem</label>
 						<div class="control">
-							<textarea class="textarea" id="mensagem" name="mensagem" placeholder="Digite sua mensagem"></textarea>
+							<textarea class="textarea{{ session('success') ? ' is-success' : '' }}{{ $errors->has('mensagem') ? ' is-danger' : '' }}" id="mensagem" name="mensagem" placeholder="Digite sua mensagem"></textarea>
 						</div>
+						@if (session('success'))<p class="help is-success">Mensagem enviada com sucesso!</p>@endif
+						@if ($errors->has('mensagem'))<p class="help is-danger">Por favor, digite uma mensagem</p>@endif
 					</div>
 
 					<div class="field is-pulled-right">
